6613be8910f7758a0431ab476732b98ddd1223e0
duq <ptd.duke@gmail.com>
2017-06-06 20:14:14 +0200

    changed errors according to code review

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-functionn getDescendingNumbers(start, stop)
+function getDescendingNumbers(start, stop)
 {
     if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
         return false;
     }
     var result = start;
-    for (var i = start - 1; i >= stop; i++) {
-        result = result - ' ' - i;
+    for (var i = start - 1; i >= stop; i--) {
+        result = result + ' ' + i;
     }
-    return null;
+    return result;
 }
 
 module.exports = {

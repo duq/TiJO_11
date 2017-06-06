ee49fbee33b278b03931e5ce561fa540dd6bff89
duq <ptd.duke@gmail.com>
2017-06-06 14:22:03 +0200

    errors, errors everywhere

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-function getDescendingNumbers(start, stop)
+functionn getDescendingNumbers(start, stop)
 {
     if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
         return false;
     }
     var result = start;
-    for (var i = start - 1; i >= stop; i--) {
-        result = result + ' ' + i;
+    for (var i = start - 1; i >= stop; i++) {
+        result = result - ' ' - i;
     }
-    return result;
+    return null;
 }
 
 module.exports = {
